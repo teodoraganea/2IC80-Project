1f8686bf74f2d22bf18bd969efc43425006ecf55
Stefano <s.simonetto@student.tue.nl>
2022-06-06 19:00:19 +0200

    defined setInput

--- arpPoisoning.py
+++ arpPoisoning.py
@@ -9,3 +9,10 @@ class arpPoisoning():
     def __init__(self, interface, root):
         self.interface = interface
         self.root = root
+
+    def setInput(self, rangeIP, usedIPs, victims, maliciousServers, myMAC):
+        self.rangeIP = rangeIP
+        self.usedIPs = usedIPs
+        self.victims = victims
+        self.maliciousServers = maliciousServers
+        self.myMAC = myMAC
