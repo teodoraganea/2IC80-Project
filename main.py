4db05227d5d699c46eb959e8885aba9bd2215432
Stefano <s.simonetto@student.tue.nl>
2022-06-06 17:34:58 +0200

    initialize the three attacks

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ root.title('ARP_DNS_SSL')
 
 mode = ""
 interface = ""
+
+def initiate():
+    if mode == "arp":
+        process = arp(interface, root)
+        process.get_IP()
+    elif mode == "dns":
+        process = dnsPoisoning(interface, root)
+        process.get_IP()
+    elif mode == "ssl":
+        process = SSLstrip(interface, root)
+        process.get_IP()
+    else:
+        print("Wrong initiation")
